2e0f0f066e03ceb38265013f42a3bc3c0c9b09b2
Sibin C Baby <41861279+sibincbaby@users.noreply.github.com>
2020-01-11 11:06:33 +0530

    Update weather.js
    2nd commit

--- weather/weather.js
+++ weather/weather.js
@@ -1,41 +1,47 @@
-$(document).ready(function () {
+<!DOCTYPE html>
+<html lang="en">
 
-    $("#submitCity").click(function () {
-        return getWeather();
-    });
+<head>
+    <meta charset="UTF-8">
+    <title>Weather App</title>
 
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
-});
+    <link rel="stylesheet" href="style.css">
 
-function getWeather() {
-    var city = $("#city").val();
+</head>
 
-    if (city != '') {
+<body>
 
-        $.ajax({
-            url: 'http://api.openweathermap.org/data/2.5/weather?q=' + city + "&units=metric" + "&APPID=a080a0d056f23d5c5ccfebee4a1ca989",
-            type: "GET",
-            dataType: "jsonp",
-            success: function (data) {
-                var widget = showResults(data)
+    <div class="container" style="height:650px;">
+        <div class="row">
+            <!--title of the app-->
+            <div class="col-md-12" style="margin-bottom:10px;">
+                <h3 class="text-center text-primary">Weather App</h3>
+                <span id="error" class="text-center"></span>
+            </div>
+            <!--text box and search button-->
+            <div class="row form-group form-inline" id="cityDiv">
 
+                <input type="text" name="city" id="city" class="form-control" placeholder="Enter city name">
+                <button id="submitCity" class="btn btn-primary">Search City</button>
+            </div>
 
-                $("#showWeather").html(widget);
+        </div>
+        <!--Getting weather info from openmap and dispaly it in this div-->
 
-                $("#city").val('');
-            }
+        <div id="showWeather"></div>
+    </div>
+    
+    
+    <!--jquery cdn-->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    <!--local js link-->
+    <script src="weather.js"></script>
 
-        });
+</body>
 
-
-    } else {
-        $("#error").html("<div class='alert alert-danger' id='errorCity'><a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a>City field cannot be empty</div>");
-    }
-
-
-}
-
-
-function showResults(data) {
-    return '<h2 style="font-weight:bold; font-size:30px; padding-top:20px;" class="text-center">Current Weather for ' + data.name + ', ' + data.sys.country + '</h2>' + "<h3 style='padding-left:40px;'><strong>Temperature</strong>: " + data.main.temp + " &deg;C</h3>"
-}
+</html>
